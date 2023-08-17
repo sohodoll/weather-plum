f63124075923988f3cf7aec572155ce2665197c5
sohodoll <kovalevvtl@gmail.com>
2023-08-17 20:55:15 +0300

    update: update vue config

--- vue.config.js
+++ vue.config.js
@@ -1,1 +1,19 @@
-module.exports = {}
+const { defineConfig } = require('@vue/cli-service')
+const webpack = require('webpack')
+
+module.exports = defineConfig({
+  transpileDependencies: true,
+  publicPath: '/weather-widget/',
+  filenameHashing: false,
+  productionSourceMap: false,
+  configureWebpack: {
+    plugins: [
+      new webpack.optimize.LimitChunkCountPlugin({
+        maxChunks: 1,
+      }),
+    ],
+  },
+  chainWebpack: (config) => {
+    config.optimization.delete('splitChunks')
+  },
+})
