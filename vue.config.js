371402773ab0ec1ed8bd61b70644fc6761adaa92
sohodoll <kovalevvtl@gmail.com>
2023-08-17 16:59:45 +0300

    update: update vue config

--- vue.config.js
+++ vue.config.js
@@ -1,27 +1,40 @@
+const { DefinePlugin } = require('webpack')
+
 module.exports = {
   transpileDependencies: true,
   publicPath: '/dist/',
   filenameHashing: false,
   productionSourceMap: false,
   configureWebpack: {
-    optimization: {
-      splitChunks: false,
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
-          use: [
-            {
-              loader: 'file-loader',
-              options: {
-                limit: Infinity,
-                name: 'fonts/[name].[hash:8].[ext]',
-              },
-            },
-          ],
-        },
-      ],
-    },
+    plugins: [
+      new DefinePlugin({
+        __VUE_OPTIONS_API__: true,
+        __VUE_PROD_DEVTOOLS__: false,
+      }),
+    ],
+  },
+  chainWebpack: (config) => {
+    config.optimization.delete('splitChunks') // Disable chunk splitting
+
+    // Exclude font files from default rules
+    config.module
+      .rule('fonts')
+      .test(/\.(woff2?|eot|ttf|otf)(\?.*)?$/)
+      .use('url-loader')
+      .loader('url-loader')
+      .options({
+        limit: Infinity,
+        esModule: false, // Disable ES modules for fonts
+        name: 'app.[hash:8].[ext]', // Include font data in app.js
+      })
+
+    // Exclude default scss rule
+    config.module
+      .rule('scss')
+      .oneOf('normal')
+      .use('sass-loader')
+      .tap((options) => ({
+        ...options,
+      }))
   },
 }
